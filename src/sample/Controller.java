e80de1a1380263c16e192496066cdc1770208bb1
pzawi <p.zawidzki665@gmail.com>
2022-04-14 17:34:40 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Controller {
+
+    @FXML
+    private Button btnOdczyt;
+
+    @FXML
+    private Button btnZapis;
+
+    @FXML
+    public TextArea txtArea;
+
+
+    @FXML
+    void odczytAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zapisAction(ActionEvent event) throws FileNotFoundException {
+
+
+
+
+        }
+    }
+
+
+
